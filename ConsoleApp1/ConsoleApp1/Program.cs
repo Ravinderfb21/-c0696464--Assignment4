542fa1d52adefacb745943a7659c47543bac9a97
Ravinder Singh <696464@student.tor.loc>
2019-04-11 16:12:12 -0400

    section A done

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,49 +11,58 @@ namespace Assignment_4
     class Program
     {
         ArrayList Beowulf;
-
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
-            p.Run();
-        }
+            p.ReadTextFiles();
+            Console.ReadKey();
 
-        public void Run() { this.ReadTextFiles(); }
+        }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
 
         public void ReadTextFiles()
         {
-           
+            // Read file using StramReader. Read file line by line
             using (StreamReader file = new StreamReader("U:/Users/696464/ravinder/Beowulf.txt"))
             {
+
                 int counter = 0;
                 string ln;
 
                 while ((ln = file.ReadLine()) != null)
                 {
-                    Console.WriteLine(ln);
+
                     Beowulf.Add(ln);
-                    counter++;
+
                 }
+
                 file.Close();
+                counter = File.ReadLines("U:/Users/696464/ravinder/Beowulf.txt").Count();
                 Console.WriteLine($"File has {counter} lines.");
+
             }
         }
 
         public int FindNumberOfBlankSpaces(string line)
         {
-           
+
             int countletters = 0;
             int countSpaces = 0;
 
             foreach (char c in line)
             {
                 if (char.IsLetter(c)) { countletters++; }
-                if (char.IsWhiteSpace(c)) { countSpaces++; }
+
+                if (char.IsWhiteSpace(c)) { countletters++; }
             }
             return countSpaces;
 
-
         }
+
     }
+
 }
